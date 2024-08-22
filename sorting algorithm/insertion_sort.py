1277ae1dd88ecf83ce216253044c691fdefc71c9
Ankit kumar <ankitk0274@gmail.com>
2024-08-23 04:57:58 +0530

    insertion sort

---
+++ sorting algorithm/insertion_sort.py
@@ -0,0 +1,13 @@
+# insertion sort
+def insertion(arr):
+    n=len(arr)
+    for i in range(1,n):
+        key=arr[i]
+        j=i-1
+        while j>=0 and arr[j]>key:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=key
+    print(arr)
+insertion([3,5,2,16,26,2])
+            
