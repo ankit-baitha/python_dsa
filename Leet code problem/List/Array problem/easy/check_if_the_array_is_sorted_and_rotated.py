c28404154dcf9176203edbe7c28acfaefb7e3880
Ankit kumar <ankitk0274@gmail.com>
2024-09-08 12:10:07 +0530

    check sorted to rotation

--- Leet code problem/List/Array problem/easy/check_if_the_array_is_sorted_and_rotated.py
+++ Leet code problem/List/Array problem/easy/check_if_the_array_is_sorted_and_rotated.py
@@ -1,9 +1,12 @@
-def sort_array(arr):
+def sort_to_rotated(arr):
     n=len(arr)
+    rotations=0
     for i in range(0,n-1):
-        if arr[i]>arr[i+1]:
+        if arr[i]>arr[(i+1)%n]:
+            rotations +=1
+        if rotations>1:
             return False
     return True
 arr = [3, 1, 4, 1, 5, 9]
-x=sort_array(arr)
+x=sort_to_rotated(arr)
 print(x)
