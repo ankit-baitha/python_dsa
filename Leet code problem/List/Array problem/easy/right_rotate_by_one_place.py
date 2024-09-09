e6f1c6622379713eecb6fff513862d8c26d80a7a
Ankit kumar <ankitk0274@gmail.com>
2024-09-10 00:26:57 +0530

    rigth rotate by one place

--- Leet code problem/List/Array problem/easy/right_rotate_by_one_place.py
+++ Leet code problem/List/Array problem/easy/right_rotate_by_one_place.py
@@ -12,3 +12,16 @@ arr=[5,9,8,1,8,6,1]
 x=rotate_one_place(arr)
 print(x)
 
+# method 2
+
+def rotate(arr):
+    n=len(arr)
+    temp=arr[-1]
+    for i in range(n-1,0,-1):
+        arr[i]=arr[i-1]
+    arr[0]=temp
+    return arr
+arr=[5,9,8,1,8,6,1]
+x=rotate_one_place(arr)
+print(x)
+
