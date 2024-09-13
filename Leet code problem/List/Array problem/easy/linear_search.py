adba2e911d0fc871d672a2a475faf9239ab42188
Ankit kumar <ankitk0274@gmail.com>
2024-09-13 10:39:37 +0530

    linear search

---
+++ Leet code problem/List/Array problem/easy/linear_search.py
@@ -0,0 +1,15 @@
+""""
+question 
+
+Our intent is to search for a specific element num in an array arr using a linear search algorithm. The goal is to determine the index of num in arr if it exists; otherwise, return -1 indicating that the element is not found in the array.
+time complexity =O(n)
+space complexity = O(1)
+"""
+def linear_search(nums,target):
+    for i in range(0,len(nums)):
+        if nums[i]==target:
+            return i
+    return -1
+nums=[3, 5, 1, 2, 9]
+x=linear_search(nums,2)
+print(x)
