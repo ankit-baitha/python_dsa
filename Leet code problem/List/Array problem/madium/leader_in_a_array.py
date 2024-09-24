ba1dafc49b9d0416f84240af3f82127835bc2b58
Ankit kumar <ankitk0274@gmail.com>
2024-09-24 23:08:49 +0530

    leader in a array

---
+++ Leet code problem/List/Array problem/madium/leader_in_a_array.py
@@ -0,0 +1,16 @@
+"""
+time complexity = O(n)
+space complexity = O(n)
+"""
+
+def leader_in_array(nums):
+    n=len(nums)
+    leader=[]
+    maxi=float("-inf")
+    for i in range(n-1,-1,-1):
+        if nums[i]>=maxi:
+            leader.append(nums[i])
+            maxi=nums[i]
+    return leader[::-1]
+nums=[16, 17, 4, 3, 5, 2]
+print(leader_in_array(nums))
