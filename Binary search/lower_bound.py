8a222a97e6e06e978d4f1bb696906b4ca8b531d7
Ankit kumar <ankitk0274@gmail.com>
2024-09-27 10:21:35 +0530

    lower bound

---
+++ Binary search/lower_bound.py
@@ -0,0 +1,14 @@
+def lower_bound(arr,target):
+    low=0
+    high=len(arr)-1
+    id =len(arr)
+    while low<=high:
+        mid =(low + high)//2
+        if target<=arr[mid]:
+            id=mid
+            high=mid-1
+        else:
+            low=mid +  1
+    print(id)
+arr=[1, 2, 4, 4, 4, 6, 7]
+lower_bound(arr,4)
