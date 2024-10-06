6330bdd2e9c2937356b6f3132bcf7b238159879b
Ankit kumar <ankitk0274@gmail.com>
2024-10-06 12:39:53 +0530

    inheritance

--- 12.opps/inheritance/inheritance_3.py
+++ 12.opps/inheritance/inheritance_3.py
@@ -3,6 +3,13 @@ class Animal:
         self.color=input("Enter color = ")
         self.sound=input("Enter sound = ")
         self.height=int(input("Enter height = "))
+    def details(self):
+        print("-----1------")
+        print(f"color = {self.color}")
+        print(f"sound = {self.sound}")
+        print(f"height = {self.height}")
+        
+        
 class Dog(Animal):
     def __init__(self):
         self.set_animal_details()
@@ -10,12 +17,21 @@ class Dog(Animal):
         self.dog_owner=input("Enter dog owner name  = ")
         
     def display(self):
+        print("------2----")
         print(f"Dog name={self.dog_name}")
         print(f"Dog name={self.dog_owner}")
         print(f"Dog name={self.color}")
         print(f"Dog name={self.sound}")
         print(f"Dog name={self.height}")
         
+    def details(self):
+        # self.display()
+        
+        #super key word
+        super().details()
+        
+
         
 d1=Dog()
-d1.display()
+d1.details()
+
