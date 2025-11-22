00de6efcb425b711de847db478cb933c518914c6
ankit kumar <ankitk0274@gmail.com>
2025-11-23 01:22:53 +0530

    2D list

---
+++ two_D list/tow_D_list.py
@@ -0,0 +1,45 @@
+
+# how to represent 2D list
+
+nums=[[5,2,3],[7,6,9],[1,9,5]]
+
+rows=len(nums)
+col = len(nums[0])
+for i in range(0,rows):
+    for j in range(0,col):
+        print(nums[i][j],end="  ")
+    print()
+print()
+# print(nums[1][1])
+
+
+# 1 print  upper triangle
+ 
+for i in range(0,rows):
+    for j in range(0,col):
+        if j>=i:
+            print(nums[i][j],end=" ")
+        else:
+            print("*",end = " ")
+    print()
+
+print()
+# 2 print lower triangle
+for i in range(0,rows):
+    for j in range(0,col):
+        if i>=j:
+            print(nums[i][j], end=" ")
+        else:
+            print("*",end=" ")
+    print()
+print()
+
+# 3 print diagonal 
+for i in range(0,rows):
+    for j in range(0,col):
+        if i==j:
+            print(nums[i][j], end=" ")
+        else:
+            print("*",end=" ")
+    print()
+print()
